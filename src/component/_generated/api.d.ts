20bece2dc9b1829f21457882aaf4753614036910
Ian Macartney <ianhmacartney@gmail.com>
2025-10-16 00:51:13 -0700

    keep jump to def

--- src/component/_generated/api.d.ts
+++ src/component/_generated/api.d.ts
@@ -8,50 +8,31 @@
  * @module
  */
 
-import type { FunctionReference } from "convex/server";
+import type * as internal_ from "../internal.js";
+import type * as lib from "../lib.js";
+import type * as time from "../time.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
 
 /**
- * A utility for referencing Convex functions in your app's public API.
+ * A utility for referencing Convex functions in your app's API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-export declare const api: {
+declare const fullApi: ApiFromModules<{
+  internal: typeof internal_;
+  lib: typeof lib;
+  time: typeof time;
+}>;
+export type Mounts = {
   lib: {
-    getServerTime: FunctionReference<"mutation", "public", {}, number>;
-    rateLimit: FunctionReference<
-      "mutation",
-      "public",
-      {
-        config:
-          | {
-              capacity?: number;
-              kind: "token bucket";
-              maxReserved?: number;
-              period: number;
-              rate: number;
-              shards?: number;
-              start?: null;
-            }
-          | {
-              capacity?: number;
-              kind: "fixed window";
-              maxReserved?: number;
-              period: number;
-              rate: number;
-              shards?: number;
-              start?: number;
-            };
-        count?: number;
-        key?: string;
-        name: string;
-        reserve?: boolean;
-        throws?: boolean;
-      },
-      { ok: true; retryAfter?: number } | { ok: false; retryAfter: number }
-    >;
     checkRateLimit: FunctionReference<
       "query",
       "public",
@@ -83,6 +64,13 @@ export declare const api: {
       },
       { ok: true; retryAfter?: number } | { ok: false; retryAfter: number }
     >;
+    clearAll: FunctionReference<
+      "mutation",
+      "public",
+      { before?: number },
+      null
+    >;
+    getServerTime: FunctionReference<"mutation", "public", {}, number>;
     getValue: FunctionReference<
       "query",
       "public",
@@ -135,16 +123,41 @@ export declare const api: {
         value: number;
       }
     >;
+    rateLimit: FunctionReference<
+      "mutation",
+      "public",
+      {
+        config:
+          | {
+              capacity?: number;
+              kind: "token bucket";
+              maxReserved?: number;
+              period: number;
+              rate: number;
+              shards?: number;
+              start?: null;
+            }
+          | {
+              capacity?: number;
+              kind: "fixed window";
+              maxReserved?: number;
+              period: number;
+              rate: number;
+              shards?: number;
+              start?: number;
+            };
+        count?: number;
+        key?: string;
+        name: string;
+        reserve?: boolean;
+        throws?: boolean;
+      },
+      { ok: true; retryAfter?: number } | { ok: false; retryAfter: number }
+    >;
     resetRateLimit: FunctionReference<
       "mutation",
       "public",
       { key?: string; name: string },
-      null
-    >;
-    clearAll: FunctionReference<
-      "mutation",
-      "public",
-      { before?: number },
       null
     >;
   };
@@ -152,15 +165,18 @@ export declare const api: {
     getServerTime: FunctionReference<"mutation", "public", {}, number>;
   };
 };
+// For now fullApiWithMounts is only fullApi which provides
+// jump-to-definition in component client code.
+// Use Mounts for the same type without the inference.
+declare const fullApiWithMounts: typeof fullApi;
 
-/**
- * A utility for referencing Convex functions in your app's internal API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = internal.myModule.myFunction;
- * ```
- */
-export declare const internal: {};
+export declare const api: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "internal">
+>;
 
 export declare const components: {};
