7bc65271aaa40ff8c915291a8ac25a95bd44d56d
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-12-05 21:11:15 +0100

    add day and week consts

--- src/client/index.ts
+++ src/client/index.ts
@@ -29,6 +29,8 @@ export type {
 export const SECOND = 1000;
 export const MINUTE = 60 * SECOND;
 export const HOUR = 60 * MINUTE;
+export const DAY = 24 * HOUR;
+export const WEEK = 7 * DAY;
 
 export function isRateLimitError(
   error: unknown,
