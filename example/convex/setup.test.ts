7f8f19f3ab92020ad626b666ae18fd1738925ac8
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-05-11 11:35:21 -0700

    add example setup test using rate-limiter

--- example/convex/setup.test.ts
+++ example/convex/setup.test.ts
@@ -1,11 +1,13 @@
 /// <reference types="vite/client" />
 import { test } from "vitest";
-export const modules = import.meta.glob("./**/*.*s");
 import { convexTest } from "convex-test";
 import schema from "./schema.js";
+import rateLimiter from "@convex-dev/rate-limiter/test";
+export const modules = import.meta.glob("./**/*.*s");
 
 export function initConvexTest() {
   const t = convexTest(schema, modules);
+  rateLimiter.register(t);
   return t;
 }
 
